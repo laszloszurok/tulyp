a18fab77881d69b2349b5f4c07baf016988fd20f
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2022-12-12 19:09:05 +0100

    rewrite google lyrics source using requests and beautifulsoup

--- src/lyrics_sources/google.py
+++ src/lyrics_sources/google.py
@@ -1,53 +1,54 @@
 import sys
+import os
 import re
-from urllib.request import Request, urlopen
-from urllib.error import URLError
-from urllib.parse import quote
+import requests
+from bs4 import BeautifulSoup
 
 from exceptions.lyrics_not_found import LyricsNotFoundError
 from utils import create_query_str
 
-HEADER = {"User-Agent": "Mozilla/5.0"}
-CLASS_NAME = r"\w{5,7} \w{4,5} \w{5,7}"  # dependent on User-Agent
-base_url = "https://www.google.com/search?q="
-
-def get_html(search_url: str, header: dict[str, str] = HEADER) -> str:
-    """Return html text from given search_url."""
-    try:
-        req = Request(search_url, data=None, headers=header)
-        req_search_url = urlopen(req)
-    except URLError:
-        print("No connection!")
-        sys.exit()
-
-    if req_search_url.code != 200:
-        print("invalid request")
-        sys.exit()
-
-    return req_search_url.read().decode("utf-8")
+BASE_URL: str = "https://www.google.com/search?q="
+HEADERS: dict[str, str] = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:79.0) Gecko/20100101 Firefox/79.0",
+    "Host": "www.google.com",
+    "Referer": "https://www.google.com/"
+}
 
 def get_lyrics(title: str, artist: str) -> str:
-    """Fetch lyrics from google and return list of strings."""
+    """Search for the given song on Google and return the lyrics.
+
+    Parse the response html of the Google search and extract the lyrics, then
+    return it as a string, keeping the formatting of the original text. A
+    LyricsNotFoundError exception is raised, if no lyrics were found.
+
+    Args:
+        title (str): The title of the song to search for.
+        artist (str): The artist of the song to search for.
+
+    Returns:
+        str: The lyrics.
+
+    Raises:
+        LyricsNotFoundError: No lyrics were found on Google.
+    """
     
-    search_url = f"{base_url}{create_query_str(title=title, artist=artist)}"
+    search_url: str = f"{BASE_URL}{create_query_str(title=title, artist=artist)}"
+    web_page: str = requests.get(url=search_url, headers=HEADERS).text
+    html = BeautifulSoup(web_page, "html.parser")
+    target_div = html.find(name="div", attrs={"jsname": "WbKHeb"})
 
-    html = get_html(search_url)
-    if isinstance(html, tuple):
-        return html[0]
+    if target_div is not None and target_div.contents:
+        lyrics_html: [str] = target_div.contents
+    else:
+        raise LyricsNotFoundError
 
-    html_regex = re.compile(
-        r"<div class='{}'>([^>]*?)</div>".format(CLASS_NAME), re.S)
+    lyrics: str = ""
 
-    text_list = html_regex.findall(html)
+    for verse_html in lyrics_html:
+        verse_html_pretty = BeautifulSoup(str(verse_html), "html.parser").prettify()
+        verse = BeautifulSoup(verse_html_pretty, "html.parser").get_text()
+        verse = os.linesep.join([line for line in verse.splitlines() if line.strip()])
+        lyrics = f"{lyrics}{verse}\n\n"
 
-    lyrics_lines = []
-    for line in text_list[1:]:
-        # lyrics must be multiline,
-        # ignore the artist info below lyrics
-        if line.count("\n") > 2:
-            lyrics_lines += line.split("\n")
+    return lyrics
 
-    if len(lyrics_lines) > 0:
-        return "\n".join(lyrics_lines)
-
-    raise LyricsNotFoundError
