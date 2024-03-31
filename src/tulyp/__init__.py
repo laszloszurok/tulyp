b317245164726422354f4062155990d032e956ef
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2024-03-31 11:21:28 +0200

    remove unnecessary check

--- src/tulyp/__init__.py
+++ src/tulyp/__init__.py
@@ -3,7 +3,6 @@
 # Credit to https://github.com/laszloszurok/tulyp
 
 import sys
-import dbus
 
 from tulyp.utils.misc import get_dbus_interface
 from tulyp.screen.ui import Screen
@@ -17,11 +16,4 @@ if len(sys.argv) > 1:
 
 def main() -> None:
     """Run tulyp."""
-    try:
-        interface = get_dbus_interface(player=player)
-    except dbus.DBusException:
-        print(f"{player} is not running")
-        sys.exit()
-
-    screen = Screen(dbus_interface=interface)
-    screen.run()
+    Screen(dbus_interface=get_dbus_interface(player=player)).run()
